2017d4ac28fe119110d12987c060d7057a808f46
victor hazbun <victorhazbun87@gmail.com>
2023-10-06 01:43:39 -0500

    Snow Flake IDs

---
+++ lib/snow_flake.rb
@@ -0,0 +1,68 @@
+class SnowFlake
+  TIMESTAMP_BITS = 41
+  NODE_ID_BITS   = 5
+  DATACENTER_ID_BITS = 5
+  SEQUENCE_BITS = 12
+
+  MAX_NODE_ID = (1 << NODE_ID_BITS) # 1024
+  MAX_DATACENTER_ID = (1 << DATACENTER_ID_BITS) # 1024
+  MAX_SEQUENCE = (1 << SEQUENCE_BITS) # 4096
+
+  attr_accessor :target_epoch
+
+  def initialize(target_epoch:, datacenter_id: 1, node_id: 1, sequence: 0)
+    raise OverflowError, "invalid node_id (#{node_id} >= #{MAX_NODE_ID})" if node_id >= MAX_NODE_ID
+    if datacenter_id >= MAX_DATACENTER_ID
+      raise OverflowError, "invalid datacenter_id (#{datacenter_id} >= #{MAX_DATACENTER_ID})"
+    end
+    raise OverflowError, "invalid sequence (#{sequence} >= #{MAX_SEQUENCE})" if sequence >= MAX_SEQUENCE
+
+    @target_epoch = target_epoch
+    @node_id = node_id % MAX_NODE_ID
+    @datacenter_id = datacenter_id % MAX_DATACENTER_ID
+    @sequence = sequence % MAX_SEQUENCE
+    @last_time = current_time
+  end
+
+  def next_id
+    time = current_time
+
+    raise InvalidSystemClockError, "(#{time} < #{@last_time})" if time < @last_time
+
+    @sequence = time == @last_time ? (@sequence + 1) % MAX_SEQUENCE : 0
+
+    @last_time = time
+
+    compose(@last_time, @datacenter_id, @node_id, @sequence)
+  end
+
+  def parse(flake_id)
+    SnowFlake.parse(flake_id, @target_epoch)
+  end
+
+  def self.parse(flake_id, target_epoch)
+    hash = {}
+    hash[:epoch_time] = flake_id >> (SEQUENCE_BITS + NODE_ID_BITS + DATACENTER_ID_BITS)
+    hash[:time] = Time.at((hash[:epoch_time] + target_epoch) / 1000.0)
+    hash[:datacenter_id] = (flake_id >> (SEQUENCE_BITS + DATACENTER_ID_BITS)).to_s(2)[-DATACENTER_ID_BITS, DATACENTER_ID_BITS].to_i(2)
+    hash[:node_id] = (flake_id >> SEQUENCE_BITS).to_s(2)[-NODE_ID_BITS, NODE_ID_BITS].to_i(2)
+    hash[:sequence] = flake_id.to_s(2)[-SEQUENCE_BITS, SEQUENCE_BITS].to_i(2)
+    hash
+  end
+
+  private
+
+  def compose(last_time, datacenter_id, node_id, sequence)
+    ((last_time - @target_epoch) << (SEQUENCE_BITS + NODE_ID_BITS + DATACENTER_ID_BITS)) +
+      (datacenter_id << (SEQUENCE_BITS + DATACENTER_ID_BITS)) +
+      (node_id << SEQUENCE_BITS) +
+      sequence
+  end
+
+  def current_time
+    Time.now.strftime('%s%L').to_i
+  end
+end
+
+class SnowFlake::OverflowError < StandardError; end
+class SnowFlake::InvalidSystemClockError < StandardError; end
---
+++ spec/lib/snow_flake_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'snow_flake'
+
+# describe SnowFlake do
+#   let(:target_epoch) { Time.new(2023, 9, 19, 0, 0, 0).strftime('%s%L').to_i }
+#   subject(:snow_flake) { described_class.new(target_epoch) }
+
+#   describe "#next_id" do
+#     it { expect(snow_flake.next_id).to eq('111') }
+#   end
+
+#   describe '#parse' do
+#   end
+# end
+
+
+describe SnowFlake do
+  let(:target_epoch) { Time.new(2023, 9, 19, 0, 0, 0).strftime('%s%L').to_i }
+  subject(:snow_flake) { described_class.new(target_epoch:) }
+
+  describe '#next_id' do
+    it 'returns a unique snowflake ID' do
+      expect(snow_flake.next_id).not_to eq(snow_flake.next_id)
+    end
+
+    it 'raises an OverflowError if the node ID is greater than or equal to 32' do
+      expect { described_class.new(target_epoch:, node_id: 32) }.to raise_error(SnowFlake::OverflowError)
+    end
+
+    it 'raises an OverflowError if the datacenter ID is greater than or equal to 32' do
+      expect { described_class.new(target_epoch:, datacenter_id: 32) }.to raise_error(SnowFlake::OverflowError)
+    end
+
+    it 'raises an OverflowError if the sequence is greater than or equal to 4096' do
+      expect { described_class.new(target_epoch:, sequence: 4096) }.to raise_error(SnowFlake::OverflowError)
+    end
+
+    it 'raises an InvalidSystemClockError if the current time is less than the last time' do
+      Timecop.freeze do
+        snow_flake.next_id
+        Timecop.travel(1.second.ago) do
+          expect { snow_flake.next_id }.to raise_error(SnowFlake::InvalidSystemClockError)
+        end
+      end
+    end
+  end
+
+  describe '#parse' do
+    let(:snow_flake_id) { snow_flake.next_id }
+    let(:hash) { snow_flake.parse(snow_flake_id) }
+    it 'parses a snowflake ID into a hash' do
+      Timecop.freeze do
+
+        expect(hash[:epoch_time]).to eq(snow_flake.next_id >> (SnowFlake::SEQUENCE_BITS + SnowFlake::NODE_ID_BITS + SnowFlake::DATACENTER_ID_BITS))
+        expect(hash[:time]).to eq(Time.at((hash[:epoch_time] + snow_flake.target_epoch) / 1000.0))
+        expect(hash[:datacenter_id]).to eq(1)
+        expect(hash[:node_id]).to eq(1)
+        expect(hash[:sequence]).to eq(1)
+      end
+    end
+  end
+end

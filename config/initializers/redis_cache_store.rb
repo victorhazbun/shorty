f231ffde41f60eb4c5e8e470cd1a17c21db0d0c3
victor hazbun <victorhazbun87@gmail.com>
2023-10-06 19:27:24 -0500

    revert to redis tls url without ssl param

--- config/initializers/redis_cache_store.rb
+++ config/initializers/redis_cache_store.rb
@@ -1,3 +1,3 @@
 Rails.application.configure do
-  config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  config.cache_store = :redis_cache_store, { url: ENV['REDIS_TLS_URL'] }
 end
